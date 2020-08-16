827b380bca688774ed35dad5cbd9a839003bcb6a
sepehr500 <sepehr411@gmail.com>
2020-08-16 13:01:36 -0400

    Added mark macros

--- keyboards/dz60/keymaps/sepehr500/keymap.c
+++ keyboards/dz60/keymaps/sepehr500/keymap.c
@@ -6,6 +6,11 @@
  * edit it directly.
  */
 
+enum custom_keycodes {
+    VIM_MARK = SAFE_RANGE,
+    VIM_GOTOMARK
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [0] = LAYOUT_60_ansi(KC_LGUI, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
                              KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, 
@@ -14,7 +19,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_LCTL, MO(1), KC_LALT, KC_SPC, MO(1), KC_RALT, KC_RGUI, KC_RCTL),
         [1] = LAYOUT_60_ansi(KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, 
                              KC_NO, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_NO, KC_NO, KC_NO, KC_NO, RESET, 
-                             KC_GRV, KC_NO, KC_VOLD, KC_VOLU, KC_MUTE, KC_MPLY, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO)
+                             KC_GRV, KC_NO, KC_VOLD, KC_VOLU, KC_MUTE, KC_MPLY, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, KC_NO, 
+                             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, VIM_MARK, VIM_GOTOMARK, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO)
 };
 
 enum combo_events {
@@ -22,12 +28,35 @@ enum combo_events {
   JK_BACKSPACE
 };
 
+
 const uint16_t PROGMEM tab_combo[] = {KC_U, KC_I, COMBO_END};
 const uint16_t PROGMEM backspace_combo[] = {KC_J, KC_K, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
   [OP_CHANGETAB] = COMBO_ACTION(tab_combo),
   [JK_BACKSPACE] = COMBO_ACTION(backspace_combo),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case VIM_MARK:
+        if (record->event.pressed) {
+          tap_code(KC_M);
+          register_code(KC_LSHIFT);
+          tap_code(KC_A);
+          unregister_code(KC_LSHIFT);
+        }         
+        break;
+    case VIM_GOTOMARK:
+        if (record->event.pressed) {
+          tap_code(KC_GRAVE);
+          register_code(KC_LSHIFT);
+          tap_code(KC_A);
+          unregister_code(KC_LSHIFT);
+        }         
+        break;
+    }
+    return true;
 };
 
 void process_combo_event(uint8_t combo_index, bool pressed) {
